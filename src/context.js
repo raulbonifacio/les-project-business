9d29d7bac5ac04e2a148209385ae1516d3ba4f0d
Raul Ribeiro Bonifácio <Raul Ribeiro Bonifcio>
2021-02-23 21:47:28 -0300

    - Globals changed to state.

--- src/context.js
+++ src/context.js
@@ -1,34 +1,14 @@
-const assert = require("assert");
-
 class Context {
-	constructor(input = {}, globals = {}) {
+	constructor(input = {}, state = {}) {
 		this.input = input;
-		this.globals = globals;
+		this.state = state;
 		this.errors = {};
 		this.output = {};
 	}
 
 	*[Symbol.iterator]() {
-		yield Object.keys(this.errors).length && this.errors;
-		yield Object.keys(this.output).length && this.output;
-	}
-
-	get appendContextOutputAndErrors() {
-
-		return (field, context) => {
-
-			assert(context instanceof Context);
-
-			const [errors, output] = context;
-
-			if (errors) {
-				this.errors[field] = errors;
-			}
-
-			if (output) {
-				this.output[field] = output;
-			}
-		};
+		yield Object.keys(this.err).length && this.err;
+		yield Object.keys(this.out).length && this.out;
 	}
 }
 
--- src/facade.js
+++ src/facade.js
@@ -1,23 +1,28 @@
 const assert = require("assert");
 const Context = require("./context");
 
-function facade(handlers = {}, globals = {}) {
-
+function facade(handlers = {}, initialState = {}) {
 	for (const [name, handler] of Object.entries(handlers)) {
 		assert(typeof handler == "function", `Handler ${name} is not a function.`);
 	}
 
 	const facade = new Proxy(handlers, {
-
 		get(handlers, handler) {
 
 			assert(typeof handlers[handler] == "function");
 
-			return (input, overrides = {}) => {
+			return (input, overrides = {}, externalContext) => {
+				const context =
+					externalContext ||
+					new Context(input, {
+						...initialState,
+						...overrides,
+						facade,
+					});
 
-				const context = new Context(input, { ...globals, ...overrides, facade });
+				handler = handlers[handler]();
 
-				return Promise.resolve(handlers[handler]()(context)).then(() => context);
+				return Promise.resolve(handler(context)).then(() => [...context]);
 			};
 		},
 	});
