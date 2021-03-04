9462cd9402e4f622184340ca707471e8f9cbcaa8
Raul Ribeiro Bonifácio <Raul Ribeiro Bonifcio>
2021-03-04 18:48:23 -0300

    - Confirmation validation fixed.

--- src/handlers/validation/validate-confirmation.js
+++ src/handlers/validation/validate-confirmation.js
@@ -1,6 +1,9 @@
 const defaultMessages = {
+	notSent: confirmationLabel => `O campo ${confirmationLabel} não foi enviado.`,
 	notEqual: (label, confirmationLabel) =>
 		`O campo ${label} difere do campo ${confirmationLabel}.`,
+	notFilled: confirmationLabel =>
+		`O campo ${confirmationLabel} não está preenchido.`,
 };
 
 function validateConfirmation(
@@ -8,7 +11,11 @@ function validateConfirmation(
 	messages = defaultMessages
 ) {
 	return ({ input, errors }, next) => {
-		if (!input.has(input) || !input.has(confirmation)) return next();
+		if (!input.has(confirmation)) {
+			errors.set(confirmation, messages.notSent(confirmationLabel));
+		} else if (String(input.get(confirmation)).length == 0) {
+			errors.set(confirmation, messages.notFilled(confirmationLabel));
+		}
 
 		if (input.get(field) != input.get(confirmation)) {
 			errors.set(field, messages.notEqual(label, confirmationLabel));
